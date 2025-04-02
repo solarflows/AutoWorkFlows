933fc587cc5e542ec1a18dc5451f9a8401a729ba
Husky <hyiy666@gmail.com>
2025-04-02 23:11:41 +0800

    pick up some new packages.

--- .github/diy/openwrt-packages/mt798x-23.05.sh
+++ .github/diy/openwrt-packages/mt798x-23.05.sh
@@ -120,11 +120,14 @@ format_git_clone_output -r https://github.com/kiddin9/luci-app-dnsfilter
 checkout_partial_code -r https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 checkout_partial_code -r https://github.com/linkease/nas-packages network/services/ddnsto
 
+checkout_partial_code -r https://github.com/openwrt/packages/ net/tailscale lang/golang
+
+# L
 # lucky             大吉多种功能结合体
 checkout_partial_code -r https://github.com/gdy666/luci-app-lucky luci-app-lucky lucky
 
 # M
-# # MosDNS            插件化可定制的DNS转发器
+# MosDNS            插件化可定制的DNS转发器
 checkout_partial_code -r https://github.com/sbwml/luci-app-mosdns -b v5 luci-app-mosdns mosdns v2dat
 
 # P
@@ -138,7 +141,7 @@ checkout_partial_code -r https://github.com/xiaorouji/openwrt-passwall2 luci-app
 checkout_partial_code -r https://github.com/immortalwrt/packages net/smartdns
 format_git_clone_output -r https://github.com/pymumu/luci-app-smartdns -b master
 
-#Z
+# Z
 # ZeroTier
 checkout_partial_code -r https://github.com/coolsnowwolf/packages net/zerotier
 checkout_partial_code -r https://github.com/immortalwrt/luci applications/luci-app-zerotier
