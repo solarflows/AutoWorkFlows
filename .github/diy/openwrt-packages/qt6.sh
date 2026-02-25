d8bc16b0685236634d93643d0b7ebf8cfa80b39a
Husky <hyiy666@gmail.com>
2026-02-25 14:49:22 +0800

    openwrt-packages:remove netspeedtest which from sirpdboy.

--- .github/diy/openwrt-packages/qt6.sh
+++ .github/diy/openwrt-packages/qt6.sh
@@ -124,7 +124,7 @@ checkout_partial_code -r https://github.com/Erope/openwrt_nezha luci-app-nezha o
 # netdata
 format_git_clone_output -r https://github.com/sirpdboy/luci-app-netdata
 # netspeedtest      网络测速
-format_git_clone_output -r https://github.com/sirpdboy/netspeedtest && mvdir netspeedtest
+# format_git_clone_output -r https://github.com/sirpdboy/netspeedtest && mvdir netspeedtest
 # nft-qos           基于nftables的qos工具(静态、动态双模式) 21.02以后不可用了
 # checkout_partial_code -r https://github.com/x-wrt/packages net/nft-qos
 # checkout_partial_code -r https://github.com/x-wrt/luci applications/luci-app-nft-qos
